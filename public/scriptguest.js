810133af9fcd176af16677a46c89c6a64cf99891
user <user@users-MacBook-Pro.local>
2020-06-15 22:16:56 -0400

    working peer connection anytime, style changes and remember last hostID on guest

--- public/scriptguest.js
+++ public/scriptguest.js
@@ -1,5 +1,8 @@
 const submit = document.getElementById("submit");
 
+
+document.querySelector('input').value = window.localStorage.getItem('lastConnected');
+
 function containerError(input) {
     const container = input.parentElement;
     container.className = "container error";
@@ -10,45 +13,24 @@ function containerSuccess() {
     container.className = "container success";
 }
 
-
-
-var createTimer = (initTime) => {
-    let start = Date.now();
-    const intTime = parseFloat(initTime)
-    const getTime = () => Date.now() - start;
-
-    return {
-        getTime: () => ((getTime() / 1000) + intTime).toString(),
-        setLatency: (secs) => {
-
-        }
-    }
-}
-// var timer;
-// var latTimer;
-
 var btn = document.querySelector('button');
-// var vid = document.querySelector('video');
-// btn.addEventListener('click', () => {
-//     // const t = timer.getTime()
-//     // console.log(t)
-//     // vid.currentTime = t;
-//     // vid.play();
-//     ;
-// })
-setTimeout(() => getAudioFromHost('090909'), 100)
 
-// latTimer = createTimer(0);
-
-///// TESTING AREA ///////
+btn.addEventListener('click', () => {
+    const val = document.querySelector('input').value;
+    
+    getAudioFromHost(val);
+    window.localStorage.setItem('lastConnected', val);
+    document.getElementById('code-view').style.display = "none";
+    document.getElementById('listen-view').style.display = "block";
+})
 
 const getAudioFromHost = (hostID) => {
-    var peer = new Peer({ host: 'localhost', path: '/peerjs', port: 8080, debug: 0 });
+    var peer = new Peer({ host: 'localhost', path: '/peerjs', port: 8080 });
     peer.on('open', function (id) {
         console.log('open~~~')
         peer.on('error', console.log)
 
-        var conn = peer.connect(hostID); /// host id generated.. replace with what input has avalibel
+        var conn = peer.connect(hostID);
         conn.on('error', console.log)
         conn.on('connection', console.log)
         conn.on('call', function (call) {
@@ -56,11 +38,9 @@ const getAudioFromHost = (hostID) => {
         });
 
         peer.on('call', function (call) {
-            // Answer the call automatically (instead of prompting user) for demo purposes
             call.answer();
             call.on('stream', (stream) => {
                 const audio = document.querySelector('audio');
-                // debugger
                 audio.srcObject = stream;
                 audio.onloadedmetadata = function (e) {
                     console.log('now playing the audio');
@@ -68,12 +48,5 @@ const getAudioFromHost = (hostID) => {
                 }
             })
         });
-        conn.on('open', function () {
-            // connected to peer!
-        });
-        console.log('wedwe')
     });
-    peer.on('connection', () => {
-        console.log('~~~connectiuoins')
-    })
 }
